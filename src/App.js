b780e326501f76b19cc22abeb0581118063c4854
caffine_Overflow <tnzajk@gmail.com>
2024-12-01 06:11:05 +0530

    Cart open and close

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
 import Cart from "./components/cart/Cart";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
