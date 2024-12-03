04eb1a639f2aa09f88ec78caa91373693ea2c3ed
caffine_Overflow <tnzajk@gmail.com>
2024-12-03 06:09:12 +0530

    showing and enabling delete of cart item

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from "react";
-import mealsImg from "../../assest/meals.jpg";
-import classes from "./Header.module.css";
+import { Fragment } from "react";
+
 import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
@@ -11,9 +12,10 @@ const Header = (props) => {
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImg} alt="You Failed to add  Img..."></img>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
 };
+
 export default Header;
